b52c32f98c0b6c75e2691e834045a18c9ae9bc3e
Ivan Lovkin <terra144481@gmail.com>
2023-08-17 16:15:11 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
 
-  cloud {
+/*  cloud {
     organization = "organization-name"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
